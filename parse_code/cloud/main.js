48164caa286fd7bce5005adad5307faa12b84f56
charliejl28 <cmj3@princeton.edu>
2015-02-21 13:01:34 -0800

    query youtube API for song name, ID and thumbnail

--- parse_code/cloud/main.js
+++ parse_code/cloud/main.js
@@ -55,14 +55,10 @@ Parse.Cloud.define("find", function(request, response) {
 
 	var i = 1;
 
-	var song = new Song();
 	var songName = "song " + i;
 	var artistName = "artist " + i;
 
-	song.save({
-		name: songName,
-		artist: artistName
-	}, {
+	createSongFromValues(songName, artistName, { 
 	  success: function(song) {
 	    // Execute any logic that should take place after the object is saved.
 	    // alert('New object created with objectId: ' + gameScore.id);
@@ -77,6 +73,30 @@ Parse.Cloud.define("find", function(request, response) {
 	});
 	
 });
+
+var createSongFromValues = function( title, artist, success, error) {
+
+	var songDictionary = {
+		name: songName,
+		artist: artistName
+	};
+
+	createSongFromDictionary(songDictionary, success, error);
+};
+
+var createSongFromDictionary = function(songDictionary, success, error) {
+
+	console.log('saving...');
+
+	var song = new Song();
+
+	console.log('new song');
+
+	song.save(songDictionary, {
+	  success: success,
+	  error: error
+	});
+}
 
 // convert youtube URLs to download URLs
 // save songs to Parse
@@ -104,43 +124,45 @@ Parse.Cloud.define("convert", function(request, response) {
 
 var getYoutubeURL = function(song, artist, success, error){
 
+	// create query
 	var query = song + " by " + artist;
 	query = query.replace(/ /g, "+");
 
+	// api key
 	var apiKey = "AIzaSyDAS29etVluZDFpSJ1nukEEQVv1PRpaGfM";
 
-	var params = {
-			part: "snippet",
-			q: query,
-			key: apiKey
-		};
-
-	console.log('params: ');
-	console.log(params);
-
+	// combine full url
 	var queryString = "part=snippet&q=" + query + "&key=" + apiKey;
+	var baseURL = "https://www.googleapis.com/youtube/v3/search";
+	var url = baseURL + "?" + queryString;
 
-	console.log(queryString);
-
-	var reqURL = "https://www.googleapis.com/youtube/v3/search";
-	var req = Parse.Cloud.httpRequest({
+	// send Request
+	Parse.Cloud.httpRequest({
 		method: "GET",
-		url: reqURL,
-		params: params,
+		url: url,
 		success: function( httpResponse ) {
-			console.log('youtube query success');
-			console.log(httpResponse);
+			var json = JSON.parse(httpResponse.text);
+			var firstResult = json.items[0];
+
+			var youtubeID = firstResult.id.videoId;
+			var title = firstResult.snippet.title;
+			var channel = firstResult.snippet.channelTitle;
+			var medThumbnail = firstResult.snippet.thumbnails.medium.url;
+
+			success(youtubeID, title, channel, medThumbnail);
 		},
 		error: function( httpResponse ) {
 			console.log('Youtube query request failed with response code: ' + httpResponse.status);
 			console.log('Error: ' + httpResponse.error.message);
+			console.log(httpResponse);
+
+			error(httpResponse.error.message);
 		}
 	});
 
-	console.log(req);
 }
 
-var convertYoutubeToDownload = function(youtubeURL, callback) {
+var convertYoutubeURLToDownload = function(youtubeURL, callback) {
 
 	console.log('converting youtube vid: ' + youtubeURL);
 
@@ -149,7 +171,13 @@ var convertYoutubeToDownload = function(youtubeURL, callback) {
 	var youtubeID = youtubeURL.substring(youtubeIDIndex);
 
 	console.log('extracted youtube ID: ' + youtubeID);
-http://www.youtube-mp3.org/a/itemInfo/?video_id=e-ORhEE9VVg&ac=www&t=grp&r=1424539677770&s=14231
+
+	downloadYoutubeID(youtubeID, callback);
+}
+
+var downloadYoutubeID = function(youtubeID, callback) {
+
+	/* DOES NOT WORK, USE CASPER */
 	var reqURL = "http://www.youtube-mp3.org/a/itemInfo/?video_id" + youtubeID + "&ac=www&t=grp&r=1424516943797&s=145890";
 
 	Parse.Cloud.httpRequest({
@@ -158,7 +186,9 @@ http://www.youtube-mp3.org/a/itemInfo/?video_id=e-ORhEE9VVg&ac=www&t=grp&r=14245
   		body: '',
   		success: function(httpResponse) {
    			console.log(httpResponse.text);
-			callback(youtubeURL);
+
+   			// REPLACE: hardcoded download url
+			callback('http://www.youtube-mp3.org/get?video_id=e-ORhEE9VVg&ts_create=1424547248&r=NjguNjUuMTY5LjIw&h2=b93d6850b962bdd4f3e29253fdcaeb2c&s=30315');
   		},
   		error: function(httpResponse) {
     		console.error('Request failed with response code ' + httpResponse.status);
@@ -168,10 +198,89 @@ http://www.youtube-mp3.org/a/itemInfo/?video_id=e-ORhEE9VVg&ac=www&t=grp&r=14245
 }
 
 var test = function() {
-	var song = "Blank Space";
-	var artist = "Taylor Swift";
 
-	getYoutubeURL(song, artist, null, null);
+	var song1 = {
+		name: "Blank Space",
+		artist: "Taylor Swift"
+	};
+
+	var song2 = {
+		name: "Dosas and Mimosas",
+		artist: "Cherub"
+	};
+
+	var song3 = {
+		name: "Party in the USA",
+		artist: "Miley Cyrus"
+	};
+
+	var song4 = {
+		name: "Power",
+		artist: "Kanye West"
+	};
+
+	var song5 = {
+		name: "Teenage Dream",
+		artist: "Katy Perry"
+	};
+
+	var songs = [song1, song2, song3, song4, song5];
+
+	fetchSongs(songs);
+};
+
+var fetchSongs = function (songs){
+
+	for (var i = songs.length - 1; i >= 0; i--) {
+		fetchSong(songs[i]);
+	};
+};
+
+var fetchSong = function(song){
+
+	var name = song.name;
+	var artist = song.artist;
+
+	getYoutubeURL(name, artist, function(youtubeID, title, channel, thumbnail) {
+		console.log('found a youtube song!');
+		console.log('[' + youtubeID + '] ' + title + ', ' + channel);
+
+		var youtubeURL = "https://www.youtube.com/watch?v=" + youtubeID;
+
+		downloadYoutubeID(youtubeID, function (downloadURL) {
+
+			console.log('received download url' + downloadURL);
+
+			var songDictionary = {
+				name: name,
+				artist: artist,
+				youtubeURL: youtubeURL,
+				downloadURL: downloadURL,
+				thumbnailURL: thumbnail
+			};
+
+			console.log('saving song');
+			console.log(songDictionary);
+
+			createSongFromDictionary(songDictionary, 
+				function(song){
+
+					console.log('success saving song!');
+					console.log(song);
+
+				},
+				function(song, err){
+
+					console.log('error saving song!');
+					console.log(err.message);
+				}
+			);
+
+		});
+	}, function(errorMessage){
+		console.log('failed to find youtube song');
+		console.log(errorMessage);
+	});
 
 	/*var youtube1 = "https://www.youtube.com/watch?v=e-ORhEE9VVg";
 	convertYoutubeToDownload(youtube1, function (downloadURL) {
